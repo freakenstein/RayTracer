c7a616a39eee8538950c9bf510d6045ee2d18c77
czhao <czhao.bupt@gmail.com>
2019-02-22 18:57:31 +0800

    class material

---
+++ RayTracing/src/material.h
@@ -0,0 +1,14 @@
+#ifndef MATERIALH
+#define MATERIALH
+
+struct hit_record
+
+#include "ray.h"
+#include "hitable.h"
+
+class material {
+public:
+	virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const = 0;
+};
+
+#endif
