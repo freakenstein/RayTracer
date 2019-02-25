6028b855f3790fa1fa466362f7738626eb24b56e
czhao <czhao.bupt@gmail.com>
2019-02-25 19:10:05 +0800

    material metal

--- RayTracing/src/main.cpp
+++ RayTracing/src/main.cpp
@@ -7,9 +7,10 @@
 #include "camera.h"
 #include <random>
 #include <functional>
-#include "drand48.h"
+//#include "drand48.h"
+#include "material.h"
 
-using namespace std;
+//using namespace std;
 
 /*
 vec3 color(const ray& r)
@@ -79,6 +80,7 @@ vec3 color(const ray& r)
 }
 */
 
+/*
 vec3 random_in_unit_sphere()
 {
 	vec3 p;
@@ -105,13 +107,38 @@ vec3 color(const ray& r, hitable *world)
 		return (1.0 - t)*vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 	}
 }
+*/
+
+vec3 color(const ray& r, hitable *world, int depth)
+{
+	hit_record rec;
+	if (world->hit(r, 0.001, FLT_MAX, rec))
+	{
+		ray scattered;
+		vec3 attenuation;
+		if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered))
+		{
+			return attenuation * color(scattered, world, depth + 1);
+		}
+		else
+		{
+			return vec3(0, 0, 0);
+		}
+	}
+	else
+	{
+		vec3 unit_direction = unit_vector(r.direction());
+		float t = 0.5*(unit_direction.y() + 1.0);
+		return (1.0 - t)*vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+	}
+}
 
 void main()
 {
-	ofstream f("test.ppm");
+	std::ofstream f("test.ppm");
 	if (!f)
 	{
-		cout << "File error!\n";
+		std::cout << "File error!\n";
 		return;
 	}
 
@@ -124,18 +151,20 @@ void main()
 	//vec3 horizontal(4.0, 0.0, 0.0);
 	//vec3 vertical(0.0, 2.0, 0.0);
 	//vec3 origin(0.0, 0.0, 0.0);
-	hitable *list[2];
-	list[0] = new sphere(vec3(0, 0, -1), 0.5);
-	list[1] = new sphere(vec3(0, -100.5, -1), 100);
-	hitable *world = new hitable_list(list, 2);
+	hitable *list[4];
+	list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
+	list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
+	list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2),0.3));
+	list[3] = new sphere(vec3(-1, 0, -1), 0.5, new metal(vec3(0.8, 0.8, 0.8),1.0));
+	hitable *world = new hitable_list(list, 4);
 	camera cam;
 	for (int j = ny-1; j >= 0; j--)
 	{
 		for (int i = 0; i < nx; i++)
 		{
 			vec3 col(0, 0, 0);
-			default_random_engine e;
-			uniform_real_distribution<double> urd(0.0, double(1- DBL_MIN));
+			std::default_random_engine e;
+			std::uniform_real_distribution<double> urd(0.0, double(1- DBL_MIN));
 			auto dice = bind(urd, e);
 			for (int s = 0; s < ns; s++)
 			{
@@ -143,7 +172,7 @@ void main()
 				float u = float(i + dice()) / float(nx);
 				float v = float(j + dice()) / float(ny);
 				ray r = cam.get_ray(u, v);
-				col += color(r, world);
+				col += color(r, world, 0);
 			}
 			col /= float(ns);
 
--- RayTracing/src/material.h
+++ RayTracing/src/material.h
@@ -1,7 +1,7 @@
 #ifndef MATERIALH
 #define MATERIALH
 
-struct hit_record
+struct hit_record;
 
 #include "ray.h"
 #include "hitable.h"
@@ -31,7 +31,7 @@ public:
 class lambertian : public material
 {
 public :
-	lambertian(const vec3& a) : albedo(a) {}
+	lambertian(const vec3& abd) : albedo(abd) {}
 	virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const
 	{
 		vec3 target = rec.p + rec.normal + random_in_unit_sphere();
@@ -46,15 +46,16 @@ public :
 class metal : public material
 {
 public:
-	metal(const vec3& a) : albedo(a) {}
+	metal(const vec3& abd, float f) : albedo(abd) { if (f < 1) fuzz = f; else fuzz = 1; }
 	virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const
 	{
 		vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-		scattered = ray(rec.p, reflected);
+		scattered = ray(rec.p, reflected + fuzz*random_in_unit_sphere());
 		attenuation = albedo;
 		return (dot(scattered.direction(), rec.normal) > 0);
 	}
 	vec3 albedo;
+	float fuzz;
 };
 
 #endif
